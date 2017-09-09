3d98dede0947c0648917020b4305e9917f0624e9
archikuus <archikuus@gmail.com>
2017-09-09 18:12:23 +0100

    Todo list is editable now!

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,29 +1,59 @@
 <template>
-    <div class="ui centered card">
-        <div class="content">
-        <div class="header">
+  <div class='ui centered card'>
+    <div class='content'>
+        <div class='header'>
             {{ todo.title }}
         </div>
-        <div class="meta">
+        <div class='meta'>
             {{ todo.project }}
         </div>
-        <div class="extra content">
-            <span class="right floated edit icon">
-                <i class="edit icon"></i>
-            </span>
-        </div>
+        <div class='extra content'>
+            <span class='right floated edit icon' v-on:click="showForm">
+            <i class='edit icon'></i>
+          </span>
         </div>
     </div>
-    <div class="ui bottom attached green basic button" v-show="todo.done">
+    <div class="content" v-show="isEditing">
+        <div class="ui form">
+            <div class="field">
+                <label for="">Title</label>
+                <input type="text" v-model="todo.title">
+            </div>
+            <div class="field">
+                <label for="">Project</label>
+                <input type="text" v-model="todo.project">
+            </div>
+        </div>
+        <div class="ui two button attached buttons">
+            <button class="ui basic blue button" v-on:click="hideForm">
+                Close X
+            </button>
+        </div>
+    </div>
+    <div class='ui bottom attached green basic button' v-show="todo.done">
         Completed
     </div>
-    <div class="ui bottom attached red basic button" v-show="!todo.done">
-        Completed
+    <div class='ui bottom attached red basic button' v-show="!todo.done">
+        Complete
     </div>
+</div>
 </template>
 
-<script>
-    export default {
-        props: ['todo']
+<script type="text/javascript">
+  export default {
+    props: ['todo'],
+    data () {
+      return {
+        isEditing: false
+      }
+    },
+    methods: {
+      showForm () {
+        this.isEditing = true
+      },
+      hideForm () {
+        this.isEditing = false
+      }
     }
+  }
 </script>
